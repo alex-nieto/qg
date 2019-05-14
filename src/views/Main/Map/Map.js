da727606c8ac1c65f5a4f361217210d5aab248a1
alex-nieto <alex.nieto7301@gmail.com>
2019-05-14 18:27:49 -0400

    added dynamic map

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,8 +1,110 @@
 import React, { PropTypes as T } from 'react'
 import classnames from 'classnames'
-import Map, {GoogleApiWrapper, Marker} from 'google-maps-react'
+const _ = require("lodash");
+const { compose, withProps, lifecycle } = require("recompose");
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+import SearchBox from "react-google-maps/lib/components/places/StandaloneSearchBox";
 
 import styles from './styles.module.css'
+
+const MapWithASearchBox = compose(
+  withProps({
+    googleMapURL:"https://maps.googleapis.com/maps/api/js?key="+GAPI_KEY+"&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+
+      this.setState({
+        bounds: null,
+        center: {
+          lat: 41.9, lng: -87.624
+        },
+        markers: [],
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          this.setState({
+            bounds: refs.map.getBounds(),
+            center: refs.map.getCenter(),
+          })
+        },
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+          const bounds = new google.maps.LatLngBounds();
+
+          places.forEach(place => {
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport)
+            } else {
+              bounds.extend(place.geometry.location)
+            }
+          });
+          const nextMarkers = places.map(place => ({
+            position: place.geometry.location,
+          }));
+          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+          this.setState({
+            center: nextCenter,
+            markers: nextMarkers,
+          });
+          // refs.map.fitBounds(bounds);
+        },
+      })
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    ref={props.onMapMounted}
+    defaultZoom={15}
+    center={props.center}
+    onBoundsChanged={props.onBoundsChanged}
+  >
+    <SearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      controlPosition={google.maps.ControlPosition.TOP_LEFT}
+      onPlacesChanged={props.onPlacesChanged}
+    >
+      <input
+        type="text"
+        placeholder="Customized your placeholder"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          marginTop: `27px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`,
+        }}
+      />
+    </SearchBox>
+    {props.markers.map((marker, index) =>
+      <Marker key={index} position={marker.position} />
+    )}
+  </GoogleMap>
+);
+
 
 export class MapComponent extends React.Component {
   _renderMarkers() {
@@ -40,17 +142,7 @@ export class MapComponent extends React.Component {
     const {children} = this.props;
 
     return (
-      <Map map={this.props.map}
-        google={this.props.google}
-        className={styles.map}
-        zoom={this.props.zoom}
-        onRecenter={this.props.onMove}
-        onDragend={this.props.onMove}
-        onClick={this.props.onClick}
-        visible={!children || React.Children.count(children) == 0}
-        >
-        {this._renderChildren()}
-      </Map>
+<MapWithASearchBox />
     )
   }
 }
